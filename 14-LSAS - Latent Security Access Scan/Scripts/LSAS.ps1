43a7de889f2d94a441966efdbec7f58efd6d151d
VishalHaibatpure <vihaibat@microsoft.com>
2020-07-30 22:57:01 +0530

    LSAS

--- 14-LSAS - Latent Security Access Scan/Scripts/LSAS.ps1
+++ 14-LSAS - Latent Security Access Scan/Scripts/LSAS.ps1
@@ -1,5 +1,5 @@
 ﻿
-#Install-Module AzureAD -Scope CurrentUser -AllowClobber -Repository PSGallery -Force
+Import-Module Az #-Scope CurrentUser -AllowClobber -Repository PSGallery -Force
 
 function Get-AzSKLSASScanStatus
 {
@@ -61,6 +61,7 @@ function Get-AzSKLSASScanStatus
         #caad
         $allPrivSPNOwners = @()
         $spnWithoutOwners = @()
+        $spnWithoutApps =  @()
         
         $identityModel 
 
@@ -69,16 +70,40 @@ function Get-AzSKLSASScanStatus
             $spnOid = $_.ObjectId
             $spnOwner = Get-AzureADServicePrincipalOwner -ObjectId $spnOid
 
-            if($spnOwner)
+            if(($spnOwner | Measure-Object).Count -gt 0)
             {
 
                 $allPrivSPNOwners += $spnOwner
             }
             else{
+               
+               #Check if linked AD application has any Owner
+               # Skip MSPIM applications
                if($_.DisplayName -ine 'MS-PIM')
                {
-                    $spnWithoutOwners +=  $_
+
+                    $spnDetails = Get-AzADServicePrincipal -ObjectId $_.ObjectId
+
+                    $appDetails = Get-AzADApplication -ApplicationId $spnDetails.ApplicationId  -ErrorAction SilentlyContinue
+
+                    if(($appDetails | Measure-Object).Count -gt 0)
+                    {
+                        $appOwners = Get-AzureADApplicationOwner -ObjectId  $appDetails.ObjectId
+
+                        if(  ($appOwners | Measure-Object).Count -eq 0)
+                        {
+                            $spnWithoutOwners +=  $_
+                        }
+                    }
+                    # SPN without application details can be enterprise apps which does not get returned with the help of Get-AzADApp
+                    else
+                    {
+                        $spnWithoutApps +=  $_
+                    }
+               
+                    
                }
+              
             }
             
         }
@@ -106,42 +131,151 @@ function Get-AzSKLSASScanStatus
         # exclude MS-PIM SPNs
         Write-Host $([Constants]::SingleDashLine)
 
+        #*****Step 1*****: Check for deleted application objects having access on subscription
+
         Write-Host "Deleted application object(s) having access on subscription:" -ForegroundColor $([Constants]::MessageType.Info)
 
         if(($unknownRAsAllScope | Measure-Object).Count -gt 0)
         {
-           Write-Host  ($unknownRAsAllScope | Select-Object ObjectId, ObjectType, RoleDefinitionName, Scope | FT -Wrap | Out-String)         
-           }
+           Write-Host  ($unknownRAsAllScope | Select-Object ObjectId, ObjectType,@{label="RoleDefinitionName";expression={if($_.RoleDefinitionName.Length -gt 20) { $_.RoleDefinitionName.Substring(0,17) + "..." } else {$_.RoleDefinitionName }}} , Scope | FT -Wrap | Out-String)         
+        }
         else
         {
              Write-Host "No deleted application object(s) found." -ForegroundColor $([Constants]::MessageType.Update)
         }
 
+         #*****Step 2*****: Check for deleted application objects having access on subscription
         Write-Host $([Constants]::SingleDashLine)
 
         Write-Host "Service principal(s) without owner(s) having access on subscription:" -ForegroundColor $([Constants]::MessageType.Info)
 
         if(($spnWithoutOwners | Measure-Object).Count -gt 0)
         {
-          Write-Host  ($spnWithoutOwners | Select ObjectId, DisplayName, RoleDefinitionName, Scope | FT -Wrap | Out-String)
+          #Write-Host  ($spnWithoutOwners | Select ObjectId, DisplayName, RoleDefinitionName, Scope | FT -Wrap | Out-String)
+          Write-Host  ($spnWithoutOwners | Select @{label="ObjectId";expression={$_.ObjectId}} , @{label="DisplayName";expression={$_.DisplayName}}, @{label="RoleDefinitionName";expression={if($_.RoleDefinitionName.Length -gt 20) { $_.RoleDefinitionName.Substring(0,17) + "..." } else {$_.RoleDefinitionName }}}, @{label="Scope";expression={$_.Scope}} | FT -Wrap | Out-String)
         }
         else
         {
              Write-Host "No Service principal(s) found without owner(s)" -ForegroundColor $([Constants]::MessageType.Update)
         }
 
+        #*******Step 3******* Check for application privileged roles but owner does not have role in subscription
         Write-Host $([Constants]::SingleDashLine)
 
         Write-Host "Application(s) with privileged roles but owner does not have role in subscription:" -ForegroundColor $([Constants]::MessageType.Info)
 
         if(($lsasUsers | Measure-Object).Count -gt 0)
         {
-             $lsasUsers
+            Write-Host  ($lsasUsers | Select @{label="ObjectId";expression={$_.ObjectId}} , @{label="DisplayName";expression={if($_.DisplayName.Length -gt 15) { $_.DisplayName.Substring(0,15) + "..." } else {$_.DisplayName}}}, @{label="UserPrincipalName";expression={$_.UserPrincipalName}}, UserType | FT -Wrap | Out-String)
         }
         else
         {
              Write-Host "No Service principal(s) found without owner(s)" -ForegroundColor $([Constants]::MessageType.Update)
         }
+
+        #*******Step 4****** Check for inactive identities 
+
+        # Get workspace id from Security Center
+        Write-Host $([Constants]::SingleDashLine)
+
+        Write-Host "Users and service principal(s) with no control plane activity on subscription in last 90 days:" -ForegroundColor $([Constants]::MessageType.Info)
+
+        
+        $workspaceAPI = "https://management.azure.com/subscriptions/$($SubscriptionId)/providers/Microsoft.Security/workspaceSettings/default?api-version=2017-08-01-preview"
+        $accessToken = $contextHelper.GetAccessToken(  "https://management.azure.com")
+        try
+        {
+            $wapiResponse = Invoke-WebRequest -Method GET -Uri $workspaceAPI  -Headers @{"Authorization" = "Bearer $accessToken"; "Content-Type"= "application/json"} -ErrorAction Ignore
+            $workspaceDetails = $wapiResponse.Content | ConvertFrom-Json
+            if($workspaceDetails.properties -and $workspaceDetails.properties.workspaceId)
+            {
+                
+                $wrDetail =Get-AzResource -ResourceId $workspaceDetails.properties.workspaceId
+
+                if(($wrDetail | Measure-Object).Count -gt 0 )
+                {
+                    $workspaceId = $wrDetail.Properties.customerId
+                    $workspaceAPI = "https://api.loganalytics.io/v1/workspaces/$WorkSpaceID/query"
+                    $accessToken = $contextHelper.GetAccessToken(  "https://api.loganalytics.io/")
+                    $activeIdentitiesQuery = "{'query': 'AzureActivity\r\n| where TimeGenerated > ago(90d) and  Type == \'AzureActivity\' and SubscriptionId == \'$($SubscriptionID)\'\r\n| summarize arg_max(TimeGenerated, *) by Caller \r\n| project Caller, TimeGenerated\r\n'}" | ConvertFrom-Json
+                    $apiResponse = Invoke-WebRequest -Method POST -Uri $workspaceAPI -Headers @{"Authorization" = "Bearer $accessToken"; "Content-Type"= "application/json"} -Body ($activeIdentitiesQuery | ConvertTo-Json -Depth 10 -Compress)
+                
+                    $activeIdentitiesDetails = $apiResponse.Content | ConvertFrom-Json
+
+                    $activeIdentities =@()
+
+                    $activeIdentitiesDetails.tables.rows | foreach-object { 
+                        [Guid] $validatedId = [Guid]::Empty;
+                        if([Guid]::TryParse($_[0], [ref] $validatedId))
+		                {
+                            $activeIdentities += @{ "SignInName" = ""; "LastActivityDate" = $_[1]; "ObjectId" = $_[0];"ObjectType"= "ServicePrincipal" }
+                        }
+                        else
+                        {
+                            $activeIdentities += @{ "SignInName" = $_[0]; "LastActivityDate" = $_[1]; "ObjectId" = "";"ObjectType"= "NonSPN" }
+                        }                    
+                     }
+
+                     # Skip management group assignments + groups + MS-PIM 
+                     $activeAssignments =  $allRAs | where {$_.Scope -notmatch "/providers/Microsoft.Management" -and $_.ObjectType -ne 'Unknown' -and $_.ObjectType -ne "Group" -and $_.DisplayName -notmatch "MS-PIM"} | Select-Object SignInName, ObjectId, ObjectType -Unique
+                     $inactiveIdenties = @()
+                     $activeAssignments | % {
+                     $identity = $_
+                         if($identity.ObjectType -eq "User")
+                         {
+                              $identityAssignment = $activeIdentities | Where-Object {$_.SignInName -eq $identity.SignInName}
+
+                              if(($identityAssignment | Measure-Object).Count -eq 0)
+                              {
+                                $inactiveIdenties += $identity.ObjectId
+                              }
+                         }
+                         elseif($_.ObjectType -eq "ServicePrincipal")
+                         {
+                            $identityAssignment = $activeIdentities | Where-Object {$_.ObjectId -eq $identity.ObjectId}
+
+                              if(($identityAssignment | Measure-Object).Count -eq 0)
+                              {
+                                $inactiveIdenties += $identity.ObjectId
+                              }
+                         }
+                         else{
+                            Write-Debug "Type $($identity.ObjectType) not supported for validation"   
+                         }
+                     }
+
+                     if(($inactiveIdenties | Measure-Object).Count -gt 0)
+                     {
+                        $inactiveIdenties = $inactiveIdenties | Select-Object -Unique
+                        $inactiveIdentityAssignments = $allRAs | Where-Object { $_.ObjectId -in $inactiveIdenties} | Select-Object DisplayName, ObjectId, ObjectType, @{label="RoleDefinitionName";expression={if($_.RoleDefinitionName.Length -gt 20) { $_.RoleDefinitionName.Substring(0,17) + "..." } else {$_.RoleDefinitionName }}}, Scope
+
+                        Write-Host  ($inactiveIdentityAssignments | Select @{label="ObjectId";expression={$_.ObjectId}} , @{label="DisplayName";expression={if($_.DisplayName.Length -gt 15) { $_.DisplayName.Substring(0,15) + "..." } else {$_.DisplayName}}} ,ObjectType, @{label="RoleDefinitionName";expression={if($_.RoleDefinitionName.Length -gt 20) { $_.RoleDefinitionName.Substring(0,17) + "..." } else {$_.RoleDefinitionName }}}, @{label="Scope";expression={$_.Scope}} | FT -Wrap | Out-String)
+                     }
+                     else
+                        {
+                                Write-Host "No inactive users and service principal(s) found" -ForegroundColor $([Constants]::MessageType.Update)
+                        }
+                }
+                else
+                {
+                    Write-Host "Activity logs workspace resource not found to validate active identities." -ForegroundColor $([Constants]::MessageType.Warning)
+                }
+            
+            }
+            else
+            {
+                Write-Host "Activity logs workspace not found to validate active identities." -ForegroundColor $([Constants]::MessageType.Warning)
+            }
+
+        }
+        catch{
+             Write-Host "Activity logs can not be checked. $($_.ErrorDetails.Message)." -ForegroundColor $([Constants]::MessageType.Warning)
+        }
+
+        Write-Host "$([Constants]::DoubleDashLine)" #-ForegroundColor $([Constants]::MessageType.Info)
+                Write-Host "$([Constants]::NextSteps)" -ForegroundColor $([Constants]::MessageType.Info)
+                Write-Host "$([Constants]::DoubleDashLine)"
+
     }
 
 }
@@ -160,14 +294,14 @@ class Constants
 					"   [1] Check for deleted application objects having access on subscription  `r`n" +
                     "   [2] Check for applications having access on subscription but no owners `r`n" +
                     "   [3] Check for applications with privileged roles but owner does not have role in subscription `r`n" +
-					"   [4] Check for users and applications having with no control plane activity using activity logs `r`n`n" +
-                    "More details about resources created can be found in the link: http://aka.ms/DevOpsKit/LSAS `r`n"
+					"   [4] Check for users and applications having with no control plane activity using activity logs `r`n`n"
+                    
     static [string] $DoubleDashLine    = "================================================================================"
     static [string] $SingleDashLine    = "--------------------------------------------------------------------------------"
     
     static [string] $NextSteps = "** Next steps **`r`n" + 
     "        a) Review highlighted identities access and remove all unwanted roles.`r`n" +
-    "        b) You can create compliance monitoring Power BI dashboard using link: http://aka.ms/DevOpsKit/TenantSecurityDashboard .`r`n" +
+    "        b) For more details refer: http://aka.ms/DevOpsKit/LSAS .`r`n" +
     "        c) For any feedback contact us at: azsksupext@microsoft.com .`r`n"
 }
 
@@ -209,13 +343,16 @@ class ContextHelper
                         # Switch context
                         if($this.currentContext.Subscription.Id -ne $SubscriptionId)
                         {
-                            $this.currentContext = Set-AzContext -SubscriptionId $SubscriptionId -ErrorAction Stop
+                            $this.currentContext = Set-AzContext -SubscriptionId $SubscriptionId -ErrorAction Stop -Force
                         }
                     }
                     else
                     {
                         Write-Host "Invalid PS context. ErrorMessage [$($_)]" -ForegroundColor $([Constants]::MessageType.Error)
                     }
+
+                    #TODO: do it one time
+                    
                 }
                 else
                 {
@@ -263,16 +400,3 @@ class ContextHelper
     }
     
 }
-
-
-
-
-
- 
-
-
-
- 
-
-
-
---
+++ 14-LSAS - Latent Security Access Scan/Scripts/Readme.md
@@ -0,0 +1,93 @@
+# LSAS - Latent Security Access Scan [In Preview]
+
+## Latent Security Access Scan
+### Contents
+- [Overview](Readme.md#overview)
+- [Using Latent Security Access Scan - Step by Step](Readme.md#setting-up-tenant-security-solution---step-by-step)
+- [Feedback](Readme.md#feedback)
+
+-----------------------------------------------------------------
+## Overview 
+We have a very user account centric view of granting and revoking access.
+
+When someone moves out of our team or project, we tend to remove them from various security groups or roles and relax with the thought that the user will not be able to access our project resources, services and other assets any more. However, we rarely truly assess all the indirect pathways of access from the person into the resources. For instance, in Azure, a user may own (or co-own) one or more applications or service principals and those may be configured with access to the subscription. So you may remove the person from the subscription RBAC (e.g., remove from Owner or Contributor) but that person may still own or possess credential of an SPN that has privileged access to the subscription. Similarly, in ADO, the same person may have created one or more service connections. Even though the person is removed from the project, they may retain access indirectly through those service connections.
+
+By identifying and cleaning up such inadvertent vectors of access, the risks from accidental or malicious misuse via this attack vector can be reduced. Moreover, apart from minimizing risk and security hygiene, these sort of issues will be very important to be careful about -- especially for the sovereign projects that we have to deliver in the upcoming years.
+
+In this project, we will create a tool to warn the corresponding asset owners (e.g., an Azure subscription owner) if their service/subscription/project is at risk due to such 'latent security access' pathways. We may cover Azure (and perhaps ADO) in the initial PoC...however, the concept will be extensible to other services (Office, Power Platform, AAD, etc.).
+
+
+
+## Using Latent Security Access Scan - Step by Step
+In this section, we will walk through the steps of Latent Security Access Scan
+
+To get started, we need the following prerequisites:
+
+
+**Prerequisite:**
+
+**Note:** Currently LSAS scan is supported only for Azure subscriptions. Scan for other assets types will be added soon.
+
+**1.** Installation steps are supported using following OS options: 	
+
+- Windows 10
+- Windows Server 2016
+
+**2.** PowerShell 5.0 or higher
+
+ Ensure that you are using Windows OS and have PowerShell version 5.0 or higher by typing **$PSVersionTable** in the PowerShell ISE console window and looking at the PSVersion in the output as shown below.) 
+ If the PSVersion is older than 5.0, update PowerShell from [here](https://www.microsoft.com/en-us/download/details.aspx?id=54616).  
+
+   ![PowerShell Version](../Images/00_PS_Version.PNG)   
+
+**3.** Install Az and Managed Service Identity Powershell Module using below command. For more details of Az installation refer [link](https://docs.microsoft.com/en-us/powershell/azure/install-az-ps)
+
+``` Powershell
+# Install Az Modules
+Install-Module -Name Az -AllowClobber -Scope CurrentUser
+
+#Install managed identity service module
+Install-Module -Name AzureAD -AllowClobber -Scope CurrentUser
+```
+
+**5.** Reader access on scanning subscription
+
+**6.** Download LSAS scan script from [here](./LSAS.ps1) to your local machine.  will help to unblock files. 
+
+[Back to top…](Readme.md#contents)
+
+**Step-1: Setup** 
+
+1. Open the PowerShell ISE and login to your Azure account (using **Connect-AzAccount**) and Set the context to subscription where solution needs to be installed.
+
+``` PowerShell
+# Login to Azure 
+Connect-AzAccount 
+
+# Connect to Azure AD account
+Connect-AzureAD
+
+# Set the context to hosting subscription
+Set-AzContext -SubscriptionId <SubscriptionId>
+```
+
+2. Run installation command with required parameters given. 
+
+``` PowerShell
+
+# Step 1: Point current path to extracted folder location and load setup script from deploy folder 
+
+. "<ScriptFolderPath>\LSAS.ps1"
+
+# Note: Make sure you copy  '.' present at the start of line.
+
+# Step 2: Run LSAS command. 
+
+Get-AzSKLSASScanStatus -SubscriptionId '<SubscriptionId>'
+
+```
+
+
+## Feedback
+
+For any feedback contact us at: azsksupext@microsoft.com 
